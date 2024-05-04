946fc1e56e596319fec4e1f09d83f04a3a0435b4
talaa <tamer.alaaeldine@gmail.com>
2024-05-05 02:47:48 +0300

    First Commit , fixed the social auth

---
+++ MeetingStats/main/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render (request,'main/home.html')
+
+def landing(request):
+    return render( request,'main/index.html')
